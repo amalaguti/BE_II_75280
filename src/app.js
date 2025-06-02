fdad9dcc80b8f1eba5c1a66b7cf0da9513fac15d
Adrian Malaguti <adrianma78@hotmail.com>
2025-06-02 19:58:38 -0300

    usersRouter

--- src/app.js
+++ src/app.js
@@ -1,19 +1,13 @@
 import express, { json, urlencoded } from "express";
 import mongoose from "mongoose";
+import usersRouter from "./routes/users.router.js";
+
 
 const app = express();
 const PORT = 8080;
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.t3lllk7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 mongoose.connect(uri, {})
@@ -23,3 +17,16 @@ mongoose.connect(uri, {})
 .catch((err) => {
   console.log(err);
 });
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/api/users", usersRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
