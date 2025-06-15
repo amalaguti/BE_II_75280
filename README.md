25043a6fcbc4c4d39ff75bc06203778ee6cf8150
Adrian Malaguti <adrianma78@hotmail.com>
2025-06-14 21:16:00 -0300

    cookies

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # BE_II_75280
+
+## MongoDB
+
+Hosted by [MongoDB Atlas](https://cloud.mongodb.com/)
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express, { json, urlencoded } from "express";
 import mongoose from "mongoose";
 import usersRouter from "./routes/users.router.js";
-
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 8080;
@@ -9,7 +9,11 @@ const PORT = 8080;
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.t3lllk7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Cookies
+app.use(cookieParser(process.env.COOKIE_SECRET))
+
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.vnuoakk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 mongoose.connect(uri, {})
 .then(() => {
   console.log("Connected to MongoDB");
@@ -18,8 +22,23 @@ mongoose.connect(uri, {})
   console.log(err);
 });
 
+app.get('/set-cookie', (req, res) => {
+  // Set a cookie with the name 'user' and the value 'adrian'
+  // The cookie is signed and httpOnly
+  res.cookie('user', 'adrian', { signed: true, httpOnly: true });
+  // Send a response with the message 'Signed cookie set'
+  res.send('Signed cookie set');
+  console.log('/set-cookie Cookies: ', req.cookies)
+  console.log('/set-cookie Signed Cookies: ', req.signedCookies)  
+});
 
 app.get("/", (req, res) => {
+    // Cookies that have not been signed
+    console.log('Cookies: ', req.cookies)
+
+    // Cookies that have been signed
+    console.log('Signed Cookies: ', req.signedCookies)
+    
   res.send("Hello World");
 });
 
